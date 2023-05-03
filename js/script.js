80f052b17c812b3fd3164a6440bffeb2f27c0403
Ivanrangelpm <rm551005@fiap.com>
2023-05-03 11:30:32 -0300

    Começando js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+//DIFERENÇA ENTRE LET/VAR/CONST
+// var nome = "Kayque";
+// if(nome == "Kayque"){
+//     let nome = "Denise";
+// }
+// console.log(nome);
+
+ //Recuperando elemento butom que esta no index html
+ const btn = document.getElementById("meu-btn");
+
+ //Atrelando um evento de click ao buttom que foi recuperado
+ btn.addEventListener( "click",  function(){
+
+    //função matematica  Math
+    let r,g,b;
+    
+
+ });
