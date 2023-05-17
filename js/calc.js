ba2e9059759d52bdd6659eb7628ccb8116cced39
Ivanrangelpm <rm551005@fiap.com.br>
2023-05-17 11:05:45 -0300

    Calc finalizada

--- js/calc.js
+++ js/calc.js
@@ -1,10 +1,21 @@
 
+
 function insereNumero(botao,visor) {
-    visor.value += botao.value;
+    
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+       
+        if(visor.value[visor.value.length-1] == "+" ||  visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/"){
+            visor.value.slice(visor.value.length-1);
+        }else{
+            visor.value += botao.value;
+        }
+
+    }else{
+        visor.value += botao.value;
+    }
 
 }
 
 function resultado(visor) {
-    
+    visor.value = eval(visor.value);
 }
-
